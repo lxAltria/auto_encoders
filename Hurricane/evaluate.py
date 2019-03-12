84798e2b005f25f60c1780dfa77a16faa2c6cae7
Xin Liang <xin@eduroam062-209.wl.anl-external.org>
2019-03-12 15:30:54 -0500

    test

--- Hurricane/evaluate.py
+++ Hurricane/evaluate.py
@@ -23,17 +23,30 @@ decoder = load_model("decoder_{}.h5".format(sys.argv[1]))
 
 encoded_train = encoder.predict(x_train)
 decoded_train = decoder.predict(encoded_train)
-decoded_train = decoded_train * value_range_train + min_train
-decoded_train = decoded_train.reshape([-1, 100, 500, 500])
 encoded_test = encoder.predict(x_test)
 decoded_test = decoder.predict(encoded_test)
+
+from assess import PSNR
+print("---------- Statistics for normalized training data ----------")
+for i in range(len(x_train)):
+	psnr, rmse = PSNR(x_train[i], decoded_train[i])
+	print("RMSE = {:.4g}, MSE = {:.4g}".format(rmse, rmse*rmse))
+print("\n\n")
+
+print("---------- Statistics for normalized testing data ----------")
+for i in range(len(x_test)):
+	psnr, rmse = PSNR(x_test[i], decoded_test[i])
+	print("RMSE = {:.4g}, MSE = {:.4g}".format(rmse, rmse*rmse))
+
+decoded_train = decoded_train * value_range_train + min_train
+decoded_train = decoded_train.reshape([-1, 100, 500, 500])
 decoded_test = decoded_test * value_range_test + min_test
 decoded_test = decoded_test.reshape([-1, 100, 500, 500])
 
 x_train, x_test = load_Hurricane_data("Uf.dat")
 x_train = x_train.reshape([-1, 100, 500, 500])
 x_test = x_test.reshape([-1, 100, 500, 500])
-from assess import PSNR
+
 print("---------- Statistics for training data ----------")
 for i in range(len(x_train)):
 	psnr, rmse = PSNR(x_train[i], decoded_train[i])
