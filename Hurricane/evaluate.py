cc03ac5ad301dab3182d05b3c5f1bd14902de688
Xin Liang <xin@eduroam062-209.wl.anl-external.org>
2019-03-12 15:20:18 -0500

    test

--- Hurricane/evaluate.py
+++ Hurricane/evaluate.py
@@ -16,7 +16,7 @@ x_test = (x_test - min_test) / value_range_test
 x_train = np.reshape(x_train, (len(x_train), 500, 500, 1))  # adapt this if using `channels_first` image data format
 x_test = np.reshape(x_test, (len(x_test), 500, 500, 1))  # adapt this if using `channels_first` image data format
 
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import sys
 encoder = load_model("encoder_{}.h5".format(sys.argv[1]))
 decoder = load_model("decoder_{}.h5".format(sys.argv[1]))
@@ -24,11 +24,15 @@ decoder = load_model("decoder_{}.h5".format(sys.argv[1]))
 encoded_train = encoder.predict(x_train)
 decoded_train = decoder.predict(encoded_train)
 decoded_train = decoded_train * value_range_train + min_train
+decoded_train.reshape([-1, 100, 500, 500])
 encoded_test = encoder.predict(x_test)
 decoded_test = decoder.predict(encoded_test)
 decoded_test = decoded_test * value_range_test + min_test
+decoded_test.reshape([-1, 100, 500, 500])
 
 x_train, x_test = load_Hurricane_data("Uf.dat")
+x_train.reshape([-1, 100, 500, 500])
+x_test.reshape([-1, 100, 500, 500])
 from assess import PSNR
 print("---------- Statistics for training data ----------")
 for i in range(len(x_train)):
