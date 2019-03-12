317cf5cc0b6736f8a8295faccee94c1ffcbb7777
Xin Liang <xin@eduroam062-209.wl.anl-external.org>
2019-03-12 11:05:30 -0500

    test

--- Hurricane/evaluate.py
+++ Hurricane/evaluate.py
@@ -1,20 +1,22 @@
 from load_data import load_Hurricane_data
-from assess import psnr
+from assess import PSNR
 import numpy as np
 
 x_train, x_test = load_Hurricane_data("Uf.dat")
 x_train = x_train.reshape([-1, 100, 500, 500])
-x_test = x_test.reshape([-1, 500, 500])
+x_test = x_test.reshape([-1, 100, 500, 500])
 dec_train = np.fromfile("/tmp/xin/Hurricane/decoded_cnn_train.dat", dtype=np.float32)
 dec_train = dec_train.reshape([-1, 100, 500, 500])
 dec_test = np.fromfile("/tmp/xin/Hurricane/decoded_cnn_test.dat", dtype=np.float32)
-dec_test = dec_test.reshape([-1, 500, 500])
+dec_test = dec_test.reshape([-1, 100, 500, 500])
 
-print("PSNR for training data")
+print("---------- Statistics for training data ----------")
 for i in range(len(x_train)):
-	print(psnr(x_train[i], dec_train[i]))
+	psnr, rmse = PSNR(x_train[i], dec_train[i])
+	print("RMSE = {:.4g}, PSNR = {:.2f}".format(rmse, psnr))
 
-print("PSNR for testing data")
+print("\n\n---------- Statistics for testing data ----------")
 for i in range(len(x_test)):
-	print(psnr(x_test[i], dec_test[i]))
+	psnr, rmse = PSNR(x_test[i], dec_test[i])
+	print("RMSE = {:.4g}, PSNR = {:.2f}".format(rmse, psnr))
 
