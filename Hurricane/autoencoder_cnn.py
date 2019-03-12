6fe36c22d6b197ad0ca4176192369de37a73e25d
Xin Liang <xin@eduroam062-209.wl.anl-external.org>
2019-03-12 10:57:27 -0500

    test

--- Hurricane/autoencoder_cnn.py
+++ Hurricane/autoencoder_cnn.py
@@ -16,69 +16,110 @@ def build_encoder():
 	encoder.add(ZeroPadding2D((6, 6), input_shape=(500, 500, 1,)))
 	# 512 * 512 * 1
 	encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 250 * 250 * 64
 	encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 125 * 125 * 64
+	# 256 * 256 * 64
+	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 63 * 63 * 128
-	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 32 * 32 * 128
+	# 128 * 128 * 128
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 16 * 16 * 256
+	# 64 * 64 * 256
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 8 * 8 * 256
-	return encoder, (500*500)*1.0 / (8*8*256)
-
-def build_encoder_simple():
-# using VGG16 architecture
-	encoder = Sequential(name="encoder")
-	# 500 * 500 * 1
-	encoder.add(Conv2D(64, (3, 3), input_shape=(500, 500, 1,), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 250 * 250 * 64
-	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((2, 2), padding='same'))
-	# 125 * 125 * 64
-	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((5, 5), padding='same'))
-	# 25 * 25 * 128
+	# 32 * 32 * 256
 	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-	encoder.add(MaxPooling2D((5, 5), padding='same'))
-	# 5 * 5 * 256
-	return encoder, (500*500)*1.0 / (5*5*512)
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((2, 2), padding='same'))
+	# 16 * 16 * 512
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((2, 2), padding='same'))
+	# 8 * 8 * 512
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((2, 2), padding='same'))
+	# 4 * 4 * 512
+	return encoder, (500*500)*1.0 / (4*4*512)
 
 def build_decoder():
 	decoder = Sequential(name="decoder")
-	decoder.add(Conv2D(256, (3, 3), input_shape=(8, 8, 256,), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), input_shape=(8, 8, 512,), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(UpSampling2D((2, 2)))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(UpSampling2D((2, 2)))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(UpSampling2D((2, 2)))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
 	decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-	decoder.add(UpSampling2D((2, 2)))
-	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
 	decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-	decoder.add(UpSampling2D((2, 2)))
 	decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
 	decoder.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
 	decoder.add(Cropping2D((6, 6)))
 	return decoder
 
+def build_encoder_simple():
+# using VGG16 architecture
+	encoder = Sequential(name="encoder")
+	# 500 * 500 * 1
+	encoder.add(Conv2D(64, (3, 3), input_shape=(500, 500, 1,), activation='relu', padding='same'))
+	encoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((2, 2), padding='same'))
+	# 250 * 250 * 64
+	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((2, 2), padding='same'))
+	# 125 * 125 * 64
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((5, 5), padding='same'))
+	# 25 * 25 * 128
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	encoder.add(MaxPooling2D((5, 5), padding='same'))
+	# 5 * 5 * 512
+	return encoder, (500*500)*1.0 / (5*5*512)
+
 def build_decoder_simple():
 	decoder = Sequential(name="decoder")
 	decoder.add(Conv2D(512, (3, 3), input_shape=(5, 5, 512,), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((5, 5)))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	decoder.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((5, 5)))
 	decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+	decoder.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
+	decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 	decoder.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 	decoder.add(UpSampling2D((2, 2)))
 	decoder.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
