2af1bba2b7f9fe3dd6bf6bf5a953b50cee41c17c
Xin Liang <xin@eduroam062-209.wl.anl-external.org>
2019-03-12 10:58:59 -0500

    test

--- Hurricane/evaluate.py
+++ Hurricane/evaluate.py
@@ -5,9 +5,9 @@ import numpy as np
 x_train, x_test = load_Hurricane_data("Uf.dat")
 x_train = x_train.reshape([-1, 100, 500, 500])
 x_test = x_test.reshape([-1, 500, 500])
-dec_train = np.fromfile("decoded_cnn_train.dat", dtype=np.float32)
+dec_train = np.fromfile("/tmp/xin/Hurricane/decoded_cnn_train.dat", dtype=np.float32)
 dec_train = dec_train.reshape([-1, 100, 500, 500])
-dec_test = np.fromfile("decoded_cnn_test.dat", dtype=np.float32)
+dec_test = np.fromfile("/tmp/xin/Hurricane/decoded_cnn_test.dat", dtype=np.float32)
 dec_test = dec_test.reshape([-1, 500, 500])
 
 print("PSNR for training data")
