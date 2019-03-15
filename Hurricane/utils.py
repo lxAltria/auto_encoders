16854c73d4e801f29e7280b0376b470474d5896f
Xin Liang <xin@eduroam062-236.wl.anl-external.org>
2019-03-15 12:44:40 -0500

    add variable input size

--- Hurricane/utils.py
+++ Hurricane/utils.py
@@ -11,7 +11,7 @@ def load_Hurricane_data(filename, folder="/tmp/xin/Hurricane", resize=False, blo
 			data = np.fromfile(fpath, dtype=np.float32)
 		else:
 			data = np.fromfile("{}/{}".format(folder, filename), dtype=np.float32)
-			data = data.reshape([-1, 500, 500, 1])
+			data = data.reshape([-1, 500, 500])
 			data = resize_data(data, block_size)
 			data.tofile(fpath)
 	num = len(data)
@@ -61,25 +61,24 @@ def resize_data(data, block_size):
 		for j in range(n):
 			for k in range(n):
 				resized_data[i, j, k] = data[i, pos[j]:pos[j] + block_size, pos[k]:pos[k] + block_size]
-	return resized_data.reshape([num * n * n, block_size, block_size])
+	return resized_data.reshape([num * n * n, block_size, block_size, 1])
 
 def synthesize_data(data, block_size):
 	n = 500 // block_size
 	if (n % block_size) != 0:
 		n = n + 1
-	num = len(data)
+	num = len(data) // n // n
 	pos = get_split_positions(block_size, n)
-	data_shape = data.shape
-	count_matrix = np.ones(data_shape[-2], data_shape[-1], dtype=np.int8)
-	for j in range(n - 1):
-		for k in range(n - 1):
-			count_matrix[pos[j+1]:pos[k] + block_size, pos[k+1]:pos[k] + block_size] = count_matrix[pos[j+1]:pos[k] + block_size, pos[k+1]:pos[k] + block_size] + 1
 	data_syn = np.zeros([num, 500, 500], dtype=np.float32)
 	tmp_data = data.reshape([num, n, n, block_size, block_size])
 	for i in range(num):
 		for j in range(n):
 			for k in range(n):
 				data_syn[i, pos[j]:pos[j] + block_size, pos[k]:pos[k] + block_size] = data_syn[i, pos[j]:pos[j] + block_size, pos[k]:pos[k] + block_size] + tmp_data[num, j, k]
-		data_syn[i] = data_syn[i] / count_matrix
+	count_matrix = np.ones([500, 500], dtype=np.int8)
+	for j in range(n - 1):
+		for k in range(n - 1):
+			count_matrix[pos[j+1]:pos[k+1] + block_size, pos[k+1]:pos[k+1] + block_size] = count_matrix[pos[j+1]:pos[k+1] + block_size, pos[k+1]:pos[k+1] + block_size] + 1
+	data_syn = data_syn / count_matrix
 	return data_syn
 
